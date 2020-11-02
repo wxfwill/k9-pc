26b6b3c1541593c33ccb215c8db2f9941552b288
Luhom <pengluhong@yeah.net>
2020-11-02 17:55:59 +0800

    报备上报新增

--- src/pages/reportManage/FourReport.js
+++ src/pages/reportManage/FourReport.js
@@ -1,5 +1,25 @@
 import React, { Component } from 'react';
-import { Row, Col, Card, Table, Input, Button, Popconfirm, Form } from 'antd';
+import {
+  Row,
+  Col,
+  Card,
+  Button,
+  Table,
+  Input,
+  Form,
+  DatePicker,
+  Radio,
+  InputNumber,
+  Select,
+  TreeSelect,
+  Popconfirm,
+  message,
+} from 'antd';
+import Moment from 'moment';
+
+const { TextArea } = Input;
+const { Option } = Select;
+const { TreeNode } = TreeSelect;
 
 const EditableContext = React.createContext();
 
@@ -14,7 +34,54 @@ const EditableFormRow = Form.create()(EditableRow);
 class EditableCell extends React.Component {
   state = {
     editing: false,
+    personnelList: [],
+    personnelTree: [],
   };
+
+  componentDidMount() {
+    this.getCombatStaff();
+    this.queryGroupUser();
+  }
+
+  //查询人员列表
+  getCombatStaff() {
+    React.httpAjax('post', config.apiUrl + '/api/userCenter/getCombatStaff')
+      .then((res) => {
+        if (res.code == 0) {
+          this.setState({
+            personnelList: res.data,
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  //查询中队及用户信息。key：中队名称；value：中队下符合条件人员；
+  queryGroupUser() {
+    let dataObj = {
+      keyword: '',
+    };
+    React.httpAjax('post', config.apiUrl + '/api/userCenter/queryGroupUser', dataObj)
+      .then((res) => {
+        if (res.code == 0) {
+          let arr = [];
+          for (const name in res.data) {
+            arr.push({
+              name: name,
+              children: res.data[name],
+            });
+          }
+          this.setState({
+            personnelTree: arr,
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   toggleEdit = () => {
     const editing = !this.state.editing;
@@ -38,9 +105,164 @@ class EditableCell extends React.Component {
 
   renderCell = (form) => {
     this.form = form;
-    const { children, dataIndex, record, title } = this.props;
-    const { editing } = this.state;
-    return editing ? (
+    const { dataIndex, record, title } = this.props;
+    let domHtml = '';
+    switch (title) {
+      case '时间':
+        domHtml = (
+          <DatePicker ref={(node) => (this.input = node)} showTime placeholder="请选择时间" onChange={this.save} />
+        );
+        break;
+      case '来源':
+        domHtml = (
+          <Radio.Group ref={(node) => (this.input = node)} onChange={this.save}>
+            <Radio value="wg">警犬大队工作群</Radio>
+            <Radio value="f2f">面对面口述</Radio>
+          </Radio.Group>
+        );
+        break;
+      case '类别':
+        domHtml = (
+          <TreeSelect
+            ref={(node) => (this.input = node)}
+            showSearch
+            style={{ width: '100%' }}
+            dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
+            placeholder="请选择类别"
+            allowClear
+            multiple
+            treeDefaultExpandAll
+            onChange={this.save}
+          >
+            <TreeNode value="parent" title="parent" key="0-1" selectable={false}>
+              <TreeNode value="0" title="类别0" key="0-1-1" />
+              <TreeNode value="1" title="类别1" key="random2" />
+              <TreeNode value="2" title="类别2" key="random2" />
+            </TreeNode>
+          </TreeSelect>
+        );
+        break;
+      case '汇报人':
+        //domHtml = <Input ref={(node) => (this.input = node)} placeholder="请输入或粘贴汇报人" onBlur={this.save} />;
+        domHtml = (
+          <Select
+            ref={(node) => (this.input = node)}
+            mode="multiple"
+            style={{ width: '100%' }}
+            placeholder="请输入或粘贴汇报人"
+            onChange={this.save}
+          >
+            {this.state.personnelList && this.state.personnelList.length > 0
+              ? this.state.personnelList.map((item) => {
+                  return <Option key={item.id}>{item.name}</Option>;
+                })
+              : null}
+          </Select>
+        );
+        break;
+      case '同行人':
+        domHtml = (
+          <TreeSelect
+            ref={(node) => (this.input = node)}
+            showSearch
+            style={{ width: '100%' }}
+            dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
+            placeholder="请选择同行人"
+            allowClear
+            multiple
+            treeDefaultExpandAll
+            onChange={this.save}
+          >
+            {this.state.personnelTree && this.state.personnelTree.length > 0
+              ? this.state.personnelTree.map((item) => {
+                  return (
+                    <TreeNode value={item.name} title={item.name} key={item.name} selectable={false}>
+                      {item.children && item.children.length > 0
+                        ? item.children.map((el) => {
+                            return <TreeNode value={el.id} title={el.name} key={el.id} />;
+                          })
+                        : null}
+                    </TreeNode>
+                  );
+                })
+              : null}
+          </TreeSelect>
+        );
+        break;
+      case '记录人':
+        //domHtml = <Input ref={(node) => (this.input = node)} placeholder="请输入或粘贴记录人" onBlur={this.save} />;
+        domHtml = (
+          <Select
+            ref={(node) => (this.input = node)}
+            mode="multiple"
+            style={{ width: '100%' }}
+            placeholder="请输入或粘贴记录人"
+            onChange={this.save}
+          >
+            {this.state.personnelList && this.state.personnelList.length > 0
+              ? this.state.personnelList.map((item) => {
+                  return <Option key={item.id}>{item.name}</Option>;
+                })
+              : null}
+          </Select>
+        );
+        break;
+      case '详细情况':
+        domHtml = (
+          <TextArea
+            ref={(node) => (this.input = node)}
+            rows={2}
+            placeholder="请输入或粘贴详细情况"
+            onBlur={this.save}
+          />
+        );
+        break;
+      case '是否反馈':
+        domHtml = (
+          <Radio.Group ref={(node) => (this.input = node)} onChange={this.save}>
+            <Radio value={1}>是</Radio>
+            <Radio value={0}>否</Radio>
+          </Radio.Group>
+        );
+        break;
+      case '反馈内容':
+        domHtml = (
+          <TextArea
+            ref={(node) => (this.input = node)}
+            rows={2}
+            placeholder="请输入或粘贴反馈内容"
+            onBlur={this.save}
+          />
+        );
+        break;
+      case '抓捕人数':
+        domHtml = <InputNumber ref={(node) => (this.input = node)} min={0} max={1000} onChange={this.save} />;
+        break;
+      case '任务地点':
+        domHtml = (
+          <Select
+            ref={(node) => (this.input = node)}
+            showSearch
+            style={{ width: 200 }}
+            placeholder="请选择任务地点"
+            optionFilterProp="children"
+            onChange={this.save}
+            filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+          >
+            <Option value="0">南山区</Option>
+            <Option value="1">福田区</Option>
+            <Option value="2">罗湖区</Option>
+          </Select>
+        );
+        break;
+      case '备注':
+        domHtml = (
+          <TextArea ref={(node) => (this.input = node)} rows={2} placeholder="请输入或粘贴备注" onBlur={this.save} />
+        );
+        break;
+    }
+    //return domHtml;
+    return (
       <Form.Item style={{ margin: 0 }}>
         {form.getFieldDecorator(dataIndex, {
           rules: [
@@ -50,24 +272,16 @@ class EditableCell extends React.Component {
             },
           ],
           initialValue: record[dataIndex],
-        })(<Input ref={(node) => (this.input = node)} onPressEnter={this.save} onBlur={this.save} />)}
+        })(domHtml)}
       </Form.Item>
-    ) : (
-      <div className="editable-cell-value-wrap" style={{ paddingRight: 24 }} onClick={this.toggleEdit}>
-        {children}
-      </div>
     );
   };
 
   render() {
     const { editable, dataIndex, title, record, index, handleSave, children, ...restProps } = this.props;
-    console.log(title, '========================');
     return (
       <td {...restProps}>
         {editable ? <EditableContext.Consumer>{this.renderCell}</EditableContext.Consumer> : children}
-
-        {/* {title == '时间' ? <EditableContext.Consumer>{() => <div>时间栏位</div>}</EditableContext.Consumer> : null}
-        {title == '来源' ? <EditableContext.Consumer>{() => <div>来源栏位</div>}</EditableContext.Consumer> : null} */}
       </td>
     );
   }
@@ -150,16 +364,17 @@ class FourReport extends Component {
         title: '备注',
         dataIndex: 'remark',
         width: '230px',
-        fixed: 'right',
         editable: true,
       },
       {
         title: '操作',
-        dataIndex: 'operation',
+        dataIndex: 'operationKey',
+        width: '80px',
+        fixed: 'right',
         render: (text, record) =>
           this.state.dataSource.length >= 1 ? (
-            <Popconfirm title="Sure to delete?" onConfirm={() => this.handleDelete(record.key)}>
-              <a>Delete</a>
+            <Popconfirm title="确定删除?" onConfirm={() => this.handleDelete(record.key)}>
+              <a>删除</a>
             </Popconfirm>
           ) : null,
       },
@@ -182,18 +397,19 @@ class FourReport extends Component {
     const { count, dataSource } = this.state;
     const newData = {
       key: count,
+      operationKey: this.state.count,
       arrestNum: 0,
-      categoryIds: [],
+      categoryIds: [], //类别
       feedbackContext: '',
       //id: 0,
       isFeedback: 0,
-      loggers: [],
-      peers: [],
+      loggers: [], //记录人
+      peers: [], //同行人
       remark: '',
       repDetail: '',
-      repTime: '111',
-      reporters: [],
-      source: '3333',
+      repTime: '',
+      reporters: [], //汇报人
+      source: '',
       taskLocation: '',
     };
     this.setState({
@@ -211,26 +427,48 @@ class FourReport extends Component {
       ...row,
     });
     this.setState({ dataSource: newData });
+    console.log(newData, 'ppppppppppppppppp');
   };
 
   //提交报备信息
   onSubmit = () => {
-    let dataObj = {
-      reports: this.state.dataSource,
-    };
-    React.httpAjax('post', config.apiUrl + '/api/report/create4wReport', dataObj)
-      .then((res) => {
-        console.log(res, '提交返回信息-------------');
-        // if (res.code == 0) {
-        //   this.props.history.push('/app/report/4wcommand');
-        //   message.success(successMess);
-        // } else {
-        //   message.error(errorMess);
-        // }
-      })
-      .catch((error) => {
-        console.log(error);
+    const { dataSource } = this.state;
+    if (dataSource && dataSource.length > 0) {
+      let arr = [];
+      dataSource.map((item) => {
+        arr.push({
+          categoryIds: item.categoryIds, //类别
+          feedbackContext: item.feedbackContext,
+          //id: 0,
+          isFeedback: item.isFeedback,
+          loggers: item.loggers, //记录人
+          peers: item.peers, //同行人
+          remark: item.remark,
+          repDetail: item.repDetail,
+          repTime: Moment(item.repTime),
+          reporters: item.reporters, //汇报人
+          source: item.source,
+          taskLocation: item.taskLocation,
+        });
       });
+      const dataObj = {
+        reports: arr,
+      };
+      React.httpAjax('post', config.apiUrl + '/api/report/create4wReport', dataObj)
+        .then((res) => {
+          if (res.code == 0) {
+            message.success('上报成功！');
+            this.props.history.push('/app/reportManage/FourReportListSearch');
+          } else {
+            message.error(res.msg);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      message.error('请新增上报内容！');
+    }
   };
 
   render() {
@@ -270,16 +508,13 @@ class FourReport extends Component {
             >
               增加行
             </Button>
-            {/* <Button onClick={this.handleAdd} type="primary" style={{ marginBottom: 16 }}>
-          Add a row
-        </Button> */}
             <Table
               components={components}
               rowClassName={() => 'editable-row'}
               bordered
               dataSource={dataSource}
               columns={columns}
-              scroll={{ x: 2670 }}
+              scroll={{ x: 2750 }}
               pagination={false}
             />
             <Button
